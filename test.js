cf15da641848ef8a8183824e2522ecc46873a37d
heyoimtayo <99360568+heyoimtayo@users.noreply.github.com>
2022-04-28 15:09:57 -0400

    added java

---
+++ test.js
@@ -0,0 +1,11 @@
+var name1 = 'Felix';
+var name2 = 'Kristen';
+
+if (name1 == 'Felix') {
+    alert('true!!!');
+}
+    else if (name2 == 'Felix') {
+        alert('true!!!');
+    } else {
+        alert('false!!!');
+    }
